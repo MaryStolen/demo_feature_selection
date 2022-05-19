49868ca4143eaaec632b91fc24be8860c6e2d8f8
MaryStolen <54001551+MaryStolen@users.noreply.github.com>
2022-05-19 12:59:49 +0200

    Add new function
    Adding new function to select the ML Method as parameter. Default parameter = Random Forest. This function can be extended as needed.

--- model.py
+++ model.py
@@ -1,5 +1,19 @@
 import numpy as np
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.svm import SVC
+
+
+def train_Model_and_apply(train_features: np.array, train_labels: np.array, test_features: np.array, modeltype: str = 'RF') -> np.array:
+    if modeltype == 'RF':
+        model = RandomForestRegressor().fit(X=train_features, y=train_labels.ravel())
+    elif modeltype == 'SVC':
+        model = SVC(kernel='linear').fit(X=train_features, y=train_labels.ravel())
+    else:
+        raise NameError('Modeltype is not implemented!')
+        return -1
+
+    result = model.predict(X=test_features)
+    return result
 
 
 def train_RF_and_apply(train_features: np.array, train_labels: np.array, test_features: np.array) -> np.array:
